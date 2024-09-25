87afe60634b922cb9da026fb4788db25f5a009dc
MD ARIFUL ISLAM <arif.syncjava@gmail.com>
2024-09-25 17:00:13 +0600

    initially done for version 1.0.0

--- src/main/java/com/arifsyncjava/restfulapi/response/Response.java
+++ src/main/java/com/arifsyncjava/restfulapi/response/Response.java
@@ -4,25 +4,40 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Map;
 
-public record Response(
-        String time,
-        Map<String,?> data) {
+public class Response {
+    private String time;
+    private Data data;
+    private Error error;
 
-    public Response(Map<String,?> data) {
-        this(getTime(), data);
+    private Response () {
+        this.time = getTime();
+    }
+
+    private Response (Data data) {
+        this();
+        this.data = data;
+    }
+
+    private Response (Error error) {
+        this();
+        this.error = error;
     }
 
 
-    private static String getTime() {
+    public static Response of (Data data) {
+        return new Response(data);
+    }
+
+    public static Response error (Error error) {
+        return new Response(error);
+    }
+
+    private static String getTime () {
         long timeStamp = System.currentTimeMillis();
-        ZonedDateTime now = ZonedDateTime.ofInstant(Instant.ofEpochMilli(timeStamp),
-                ZoneId.of("UTC"));
-        return now.format(DateTimeFormatter.ofPattern("hh:mm:ss-EEEE,dd MMMM,yyyy"));
+        ZonedDateTime now = ZonedDateTime
+                .ofInstant(Instant.ofEpochMilli(timeStamp), ZoneId.of("UTC"));
+        return now.format(DateTimeFormatter.ISO_DATE_TIME);
     }
-
-
 
 }
-
